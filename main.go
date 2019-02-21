82395d3363d30e3d428e4cee563bfa4c7d5c1adb
archer <archer.vanderwaal@gmail.com>
2019-02-22 00:03:34 +0800

    😋jadetrans project initilization.

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"flag"
+	"github.com/aybabtme/rgbterm"
+	"github.com/archervanderwaal/jadetrans/utils"
+)
+
+const (
+	Version                = "1.0"
+	Usage                  = "Usage of jadetrans: jadetrans <Sentences to be translated> <command>"
+	IllegalParametersError = "Illegal parameters error."
+	Logo                   = `
+       __          __   ______                     
+      / /___ _____/ /__/_  __/________ _____  _____
+ __  / / __ / __  / _ \/ / / ___/ __ / __ \/ ___/
+/ /_/ / /_/ / /_/ /  __/ / / /  / /_/ / / / (__  )
+\____/\__,_/\__,_/\___/_/ /_/   \__,_/_/ /_/____/
+  ::一北::                                 <1.0>
+`
+)
+
+var (
+	voice bool
+	engine string
+	help bool
+	version bool
+)
+
+func init() {
+	flag.BoolVar(&help, "help", false, "Show usage and exit")
+	flag.BoolVar(&version, "version", false, "Show version and exit")
+	flag.StringVar(&engine, "engine", "youdao", "Set translate engine [youdao, google]")
+	flag.BoolVar(&voice, "voice", false, "Set up the sound to read aloud")
+	flag.Usage = usage
+	flag.Parse()
+}
+
+func main()  {
+	words, _ := utils.ParseArgs(os.Args)
+	if version {
+		showVersion()
+	} else if help {
+		flag.Usage()
+	} else if len(words) < 1 {
+		showIllegalParametersError()
+		flag.Usage()
+	}
+}
+
+func usage() {
+	// #00FF00
+	logo := rgbterm.FgString(Logo, 0, 255, 0)
+	// #FF42E1
+	usage := rgbterm.FgString(Usage, 255, 66, 225)
+	fmt.Fprintf(os.Stderr, fmt.Sprintf("%s\n\n%s\n", logo, usage))
+	flag.PrintDefaults()
+}
+
+func showVersion() {
+	version := rgbterm.FgString(Version, 0, 255, 0)
+	fmt.Println(version)
+}
+
+func showIllegalParametersError() {
+	// #FF0000
+	inputIllegalParametersError := rgbterm.FgString(IllegalParametersError, 255, 0, 0)
+	fmt.Fprintf(os.Stderr, inputIllegalParametersError)
+}
---
+++ utils/utils.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"strings"
+)
+
+// ParseArgs are defined for parsing parameters,
+// and return values are the sentences or words and query parameters to be queried.
+func ParseArgs(osArgs []string) ([]string, []string) {
+	words := make([]string, 0)
+	args := []string{osArgs[0]}
+	lastArg := ""
+	for _, arg := range osArgs[1:] {
+		if strings.HasPrefix(arg, "-") {
+			args = append(args, arg)
+			lastArg = arg
+			continue
+		}
+		if strings.HasPrefix(lastArg, "-") && len(words) != 0 {
+			continue
+		}
+		words = append(words, arg)
+		lastArg = arg
+	}
+	return words, args[1:]
+}
